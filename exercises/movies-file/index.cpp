df6b13559be72eda33e2073171114e856fccb289
Felipe Lima Santos <felipeex@MacBook-Air-de-Felipe.local>
2025-02-19 23:00:27 -0300

    feat(exercises/list-one): create count for years.

--- exercises/movies-file/index.cpp
+++ exercises/movies-file/index.cpp
@@ -3,7 +3,7 @@
 
 struct Year {
   int year;
-  int countTimes;
+  int count;
 };
 
 int main() {
@@ -18,6 +18,9 @@ int main() {
 
   int quantityOfMovies = 0, countDuration = 0;
   float averageDuration = 0;
+
+  const int QUANTITY_OF_YEARS = 20;
+  Year years[QUANTITY_OF_YEARS];
 
   do {
     fscanf(movies, "%[^,],%d,%[^,],%f,%f,%d,%f\n", movieName, &year, classfication, &spent, &income, &duration, &spectates);
@@ -34,11 +37,27 @@ int main() {
       strcpy(movieWithMostSpectates, movieName);
     }
 
+    int index = 0;
+
+    while(index < QUANTITY_OF_YEARS && years[index].year != year)
+      index++;
+
+    if (index < QUANTITY_OF_YEARS) {
+      years[index].count++;
+    } else {
+      years[index].year = year;
+      years[index].count = 1;
+    }
+
     countDuration += duration;
     quantityOfMovies++;
   } while((!feof(movies)));
 
-  printf("%d", quantityOfMovies);
+  for(int index = 0; index < QUANTITY_OF_YEARS; index++) {
+    if (years[index].year) {
+      printf("%d %d", years[index].year, years[index].count);
+    }
+  }
 
   averageDuration = (float) countDuration / quantityOfMovies;
 
