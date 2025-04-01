ea8abc19306f1b8d09065dde202c08015ab14073
Felipe Lima Santos <felipeex@MacBook-Air-de-Felipe.local>
2025-03-31 21:47:40 -0300

    studying for test

---
+++ studying-for-test/index.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // unidade de tempo = segundo
+  // ler uma nova operação a cade 3 unidade de tempo.
+
+  // tempo de espera = intervalo de tempo entre a leitura da nova operação, e a hora que ela foi inicializada.
+  
+  // apenas uma operação pode ser exercutada por vez.
+}
