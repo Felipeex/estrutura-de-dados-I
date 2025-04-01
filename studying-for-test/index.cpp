0f9af3c3aa949918c540b8fafb9d4a32561ea09f
Felipe Lima Santos <felipeex@MacBook-Air-de-Felipe.local>
2025-03-31 21:52:47 -0300

    detalhando exercicio.

--- studying-for-test/index.cpp
+++ studying-for-test/index.cpp
@@ -7,4 +7,8 @@ int main() {
   // tempo de espera = intervalo de tempo entre a leitura da nova operação, e a hora que ela foi inicializada.
   
   // apenas uma operação pode ser exercutada por vez.
+
+  /* caso tenha uma operação sendo executada, e uma operação de prioridade maior for lida, 
+  a operação que está sendo executada vai parar e voltar para fila. 
+  Sendo assim, a operação com prioridade maior assumira a execução */
 }
